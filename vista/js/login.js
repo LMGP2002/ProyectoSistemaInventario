e687040ef78dfc3729b7c64b7b8ab8530ef1f2e6
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2022-10-23 21:22:57 -0500

    login

---
+++ vista/js/login.js
@@ -0,0 +1,3 @@
+document.querySelector('#show-modal').addEventListener('click',()=>{
+    document.querySelector('.modal-container').classList.add("modal-container-active")
+});
--- vista/php/index.php
+++ vista/php/index.php
@@ -8,17 +8,41 @@ include("layout/header.php");
                     Sistema de Inventario
                 </h1>
                 <p>Villa Monguí (Café-Arte)</p>
-                <a href="#" class="btn">Ingresar ahora</a>
+                <a href="#modal-container" class="btn" id="show-modal">Ingresar ahora</a>
             </div>
     
             <div class="inicio-img">
                 <img src="../assets/main.png" alt="Café Arte">
             </div>
+        
 </section>
 </main>
+<div class="modal-container">
+            <div class="modal">
+                <div class="close-btn">&times;</div>
+                    <div class="form">
+                    <h2>Ingresar al sistema</h2>
+                    
+                        <div class="form-element">
+                            <label class="label-titulo" for="usuario">Usuario:</label>
+                            <input data-cant class="inputN" name="usuario" type="text" id="usuario" placeholder="Ingrese su usuario">
+                        </div>
+                        <div class="form-element">
+                            <label class="label-titulo" for="password">Contraseña:</label>
+                            <input data-prec class="inputN" name="password" type="text" id="password" placeholder="Ingrese su contraseña">
+                        </div>
+                        
+                        
+                        <div class="form-element">
+                            <button id="btningresar" name="btningresar" type="button">Ingresar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
 
+        </div>
 <script src="../js/estilos.js"></script>
+<script src="../js/login.js"></script>
 
 </body>
 </html>
-   
