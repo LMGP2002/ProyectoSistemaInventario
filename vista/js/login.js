bd1e9ba7cf220846b6f88e8a1405c2b73fb1bd15
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2022-10-26 21:10:47 -0500

    primera version login

--- vista/js/login.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#show-modal').addEventListener('click',()=>{
-    document.querySelector('.modal-container').classList.add("modal-container-active")
-});
-document.querySelector('.modal .close-btn').addEventListener('click',()=>{
-    document.querySelector('.modal-container').classList.remove("modal-container-active")
-});
---
+++ vista/php/layout/inclLogin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!----======== CSS ======== -->
+    
+    
+    <!----===== Boxicons CSS ===== -->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <!----======== secciones ======== -->
+    <link rel="stylesheet" href="../css/sectionLogin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <link rel="icon" href="../assets/cafe.png">
+    <title>Sistema de Inventario Villa Monguí</title> 
+</head>
