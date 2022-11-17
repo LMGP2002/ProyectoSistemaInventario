de2aea60f27f34a0b33eb979ddd2fa818909c33d
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2022-11-17 11:01:23 -0500

    s

--- vista/php/reportes.php
+++ vista/php/reportes.php
@@ -2,23 +2,7 @@
 include("../../controlador/validarSesion.php");
 include("layout/header.php");
 ?>
-<form action="reporProduc.php"  method="POST"> 
-        <div class="row">
-            <div class="col-md-3">
-                 <div class="form-group">
-                    <label for="min"data-dark>Desde</label>
-                    <input type ="date" value="<?php echo date('y-m-d');?>" name ="desde" id="desde">
-                 </div>
-             </div>  
-             <div class="col-md-3">
-                 <div class="form-group">
-                    <label for="min"data-dark>Hasta</label>
-                    <input type ="date" value="<?php echo date('y-m-d');?>" name ="hasta" id="hasta">
-                 </div>
-                 <button type="submit">
-             </div> 
-       </div>
-</form>
+
 
 <div class="table">
             <div class="table_header">
@@ -37,7 +21,23 @@ include("layout/header.php");
                     <tbody>
                     <tr>
                         <th><a  href="repor.php" target="_blank"><img src="../assets/pdf.png" /></a></th>
-                        <th><a  href="reporProduc.php" target="_blank"><img src="../assets/pdf.png" /></a></th>
+                        <th><form action="reporProduc.php"  method="POST"> 
+        <div class="row">
+            <div class="col-md-3">
+                 <div class="form-group">
+                    <label for="min"data-dark>Desde</label>
+                    <input type ="date" value="<?php echo date('y-m-d');?>" name ="desde" id="desde">
+                 </div>
+             </div>  
+             <div class="col-md-3">
+                 <div class="form-group">
+                    <label for="min"data-dark>Hasta</label>
+                    <input type ="date" value="<?php echo date('y-m-d');?>" name ="hasta" id="hasta">
+                 </div>
+                 <button type="submit">
+             </div> 
+       </div>
+</form></th>
                     </tr>
                     </tbody>           
                 </table>
