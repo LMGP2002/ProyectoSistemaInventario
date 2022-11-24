3e4c86127fcd51c9d02f5f7305f0a06a2abce02b
MiguelLopezCa <90941397+MiguelLopezCa@users.noreply.github.com>
2022-11-24 00:59:39 -0500

    b

--- controlador/reporProduc.php
+++ controlador/reporProduc.php
@@ -1,8 +1,10 @@
+
 <?php
 require("./fpdf/fpdf.php");
 
 class PDF extends FPDF
 {
+ 
 // Cabecera de página
 function Header()
 {
@@ -19,20 +21,24 @@ function Header()
      $this->Cell(0, 5, 'CAFE ARTE VILLA MONGUI', 0, 1, 'C');
      $this->Cell(0, 5, 'REPORTE DE  PROVEEDORES', 0, 1, 'C');
      $this->Cell(0, 5, utf8_decode("DUITAMA-BOYACÁ"), 0, 1, 'C');
-     $this->Cell(0, 5,date('d/m/Y'),0,1,'C');
-     $this->Cell(0, 5,date("h:i:s"),0,1,'C');
+     $this->Cell(170, 5,'Fecha del reporte:',0,0,'C');
+     $this->Cell(-120, 5,date('d/m/Y'),0,1,'C');
+     $this->Cell(170, 5,'Hora del reporte:',0,0,'C');
+     $this->Cell(-120, 5,date("h:i:s"),0,0,'C');
      $this->Ln(5);
-     
+    
      
      $this->Ln(5);
     $this->Cell(190, 5,'Listado de productos',0,1,'C');
+
     $this-> Cell(18,6,'Producto',1,0,'c',0);    
     $this-> Cell(35,6,'Proveedor',1,0,'c',0); 
-    $this->Cell(30,6,'Fecha de ingreso',1,0,'c',0); 
-    $this-> Cell(30,6,'Fecha de salida',1,0,'c',0); 
-    $this-> Cell(30,6,'precio de compra',1,0,'c',0); 
-    $this->Cell(30,6,'precio de venta',1,1,'c',0); 
-
+    $this->Cell(25,6,'Fecha ingreso',1,0,'c',0);
+    $this-> Cell(19,6,'# entradas',1,0,'c',0);
+    $this-> Cell(27,6,'Precio compra',1,0,'c',0); 
+    $this-> Cell(25,6,'Fecha salida',1,0,'c',0);
+    $this-> Cell(17,6,'# salidas',1,0,'c',0);
+    $this->Cell(27,6,'Precio venta',1,1,'c',0); 
    
 }
 
@@ -59,30 +65,35 @@ if (isset($_POST["desde"]) && isset($_POST["hasta"]) ) {
 }
     require "../modelo/conexion.php";
     if (empty($desde) || empty($hasta) ) {
-    $consulta = "SELECT nombre,nom_prov, fecha_entrada,fecha_salida,precio_comp,precio_venta FROM `elemento`,`proveedor`,`entrada`,`salida` 
+    $consulta = "SELECT nombre,nom_prov, fecha_entrada,entrada.cant AS cantE,fecha_salida,salida.cant_elem_sal AS cantS,precio_comp,precio_venta FROM `elemento`,`proveedor`,`entrada`,`salida` 
     WHERE elemento.codigo= entrada.codigo_elemento and elemento.codigo=salida.codigo_elemento and proveedor.id=entrada.id_prov GROUP BY entrada.id_entrada;";
       
    }else{
-    $consulta = "SELECT nombre,nom_prov, fecha_entrada,fecha_salida,precio_comp,precio_venta FROM `elemento`,`proveedor`,`entrada`,`salida` 
+    $consulta = "SELECT nombre,nom_prov, fecha_entrada,entrada.cant AS cantE,fecha_salida,salida.cant_elem_sal AS cantS,precio_comp,precio_venta FROM `elemento`,`proveedor`,`entrada`,`salida` 
     WHERE elemento.codigo= entrada.codigo_elemento and elemento.codigo=salida.codigo_elemento and proveedor.id=entrada.id_prov and 
     salida.fecha_salida BETWEEN'$desde' AND '$hasta' and entrada.fecha_entrada BETWEEN '$desde' AND '$hasta'GROUP BY entrada.id_entrada;";  
-     
-  
   }
   $resultado= $pdo-> query($consulta); 
   // Creación del objeto de la clase heredada
 $pdf = new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Times','',12);
-  while ($mostrar= $resultado->fetch(PDO::FETCH_ASSOC)){
+$pdf->SetFont('Times','',10);
+
+
+while ($mostrar= $resultado->fetch(PDO::FETCH_ASSOC)){
+
+  
 
     $pdf-> Cell(18,6,$mostrar['nombre'],1,0,'c',0);    
     $pdf-> Cell(35,6,$mostrar['nom_prov'],1,0,'c',0); 
-    $pdf-> Cell(30,6,$mostrar['fecha_entrada'],1,0,'c',0); 
-    $pdf-> Cell(30,6,$mostrar['fecha_salida'],1,0,'c',0); 
-    $pdf-> Cell(30,6,$mostrar['precio_comp'],1,0,'c',0); 
-    $pdf-> Cell(30,6,$mostrar['precio_venta'],1,1,'c',0); 
+    $pdf-> Cell(25,6,$mostrar['fecha_entrada'],1,0,'c',0); 
+    $pdf-> Cell(19,6,$mostrar['cantE'],1,0,'c',0);
+    $pdf-> Cell(27,6,$mostrar['precio_comp'],1,0,'c',0);
+    $pdf-> Cell(25,6,$mostrar['fecha_salida'],1,0,'c',0); 
+    $pdf-> Cell(17,6,$mostrar['cantS'],1,0,'c',0);  
+    $pdf-> Cell(27,6,$mostrar['precio_venta'],1,1,'c',0);
+    
     
 }
 //$pdf-> Cell(0, 5,'Reporte desde: '+$desde +'Hasta: '+$hasta, 0, 1, 'C');
